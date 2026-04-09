04a1944134282bfccecfcd878e27fc6b0ea1c570
Andrea Santamaria <santamariaandrea048@gmail.com>
2026-04-09 11:23:38 -0500

    password

--- password_manager.py
+++ password_manager.py
@@ -1,11 +1,16 @@
 import csv
 
+
 from caesar import caesar_encrypt
 
 
 def encrypt_single_pass(filename: str) -> None:
     """TODO: Parte 1."""
-    pass
+    with open(filename, "r") as file:
+        password= file.read().strip()
+    encrypted_password = caesar_encrypt(password)
+    with open(filename,"w") as file:
+        file.write(encrypted_password)
 
 
 def encrypt_passwords_in_file(filename: str) -> None:
@@ -21,3 +26,6 @@ def change_password(filename: str, website: str, password: str) -> bool:
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
     """TODO: Parte 4."""
     pass
+
+if __name__ == "__main__":
+    encrypt_single_pass("example1.txt")
